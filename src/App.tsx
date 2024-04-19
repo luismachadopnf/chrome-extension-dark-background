8e886563d2ba1ccc14cc19b63df102247ec125fd
Luis Machado <luismachadopnf@gmail.com>
2024-04-19 13:14:53 +0200

    Popup UI with title and button, and logic to change the body color

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const DEFAULT_COLOR = "transparent";
+  const DARK_COLOR = "lightslategrey";
+
+  async function onClick() {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      currentWindow: true,
+    });
+    chrome.scripting.executeScript<string[], void>({
+      target: { tabId: tab.id! },
+      args: [DEFAULT_COLOR, DARK_COLOR],
+      func: (defaultColor, darkColor) => {
+        const currentColor = document.body.style.backgroundColor;
+
+        document.body.style.backgroundColor =
+          currentColor === darkColor ? defaultColor : darkColor;
+      },
+    });
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Dark background</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => onClick()}>change color</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
